9cb836513667601b2768298fbe5350a1c4114685
mishraagastaya196-ai <mishraagastaya196@gmail.com>
2026-05-11 17:41:20 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,17 +1,20 @@
 import os, subprocess
 
-# Video Direct Link
-VIDEO_URL = "https://io.gy/1zNwUOV5BE0JhcNoeiTqoKJ7zAcwdT_LE"
+# Video Direct Download Link (Converted from your Drive link)
+VIDEO_URL = "https://docs.google.com/uc?export=download&id=1zNwUOV5BE0JhcNoeiTqoKJ7zAcwdT_LE"
+
 # Stream Key (Secrets se aayegi)
 SK = os.getenv("STREAM_KEY")
 
 def start():
+    # ffmpeg command for YouTube Live
     cmd = (
-        f"ffmpeg -re -stream_loop -1 -i '{VIDEO_URL}' "
-        f"-c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k "
-        f"-pix_fmt yuv420p -g 50 -c:a aac -b:a 128k -ar 44100 "
-        f"-f flv rtmp://a.rtmp.youtube.com/live2/{SK}"
+        f'ffmpeg -re -stream_loop -1 -i "{VIDEO_URL}" '
+        f'-c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k '
+        f'-pix_fmt yuv420p -g 50 -c:a aac -b:a 128k -ar 44100 '
+        f'-f flv rtmp://a.rtmp.youtube.com/live2/{SK}'
     )
+    
     while True:
         print("Starting Stream...")
         subprocess.run(cmd, shell=True)
@@ -19,3 +22,5 @@ def start():
 if __name__ == "__main__":
     start()
 
+
+ 
